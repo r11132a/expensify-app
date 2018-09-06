408dfb428a57e7025cb8b16fb94408808771b133
AJ Harrington <r11132a@gmail.com>
2018-09-06 08:41:48 -0400

    Setup production webpack build

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -19,7 +19,7 @@ export class AddExpensePage extends React.Component {
   }
 }
 
-const mapDispatchToProps = () => ({
+const mapDispatchToProps = (dispatch) => ({
   addExpense: (expense) => dispatch(addExpense(expense))
 });
 
